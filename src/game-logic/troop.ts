e380faeb21d31a84dd1058bea797c51d6dd9cdce
Victor Mak <contact@htetaung.com>
2024-11-16 00:15:59 +0700

    feat: base class and enum setup

---
+++ src/game-logic/troop.ts
@@ -0,0 +1,12 @@
+import { Coordinate } from "./coordinate";
+
+export enum TroopType{
+    INFANTRY, //highest damage unit on the battlefield
+    ARCHER, //longest range unit on the battlefield
+    CAVALRY //fastest moving unit on the battlefiled
+}
+
+export enum Faction{
+    KINGDOM,    //Blue team
+    EMPIRE      //Red team
+}
